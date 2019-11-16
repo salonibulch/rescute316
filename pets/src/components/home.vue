79e1d8da251d989c4ad720c401bb0d9c0b9c7df5
Saloni Bulchandani <sb501@duke.edu>
2019-11-16 12:56:01 -0500

    compiles but broken

--- pets/src/components/home.vue
+++ pets/src/components/home.vue
@@ -2,6 +2,7 @@
     <div id="home">
           <h1><b>All pets</b></h1>
           <router-link class="routerLink" to='/profile'><a id="profile">Click for example pet</a></router-link>
+            <router-link class="routerLink" to='/login'><a id="login">Click for login</a></router-link>
     </div>
 </template>
 
